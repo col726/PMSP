acccb9679447f7d8acd6a71b80b9bcbe3479c309
Adam Himes <adamatdrexel@gmail.com>
2013-05-21 18:29:16 -0400

    rolling back

--- PMSP_CSharpClient/PMSPClient/Track.cs
+++ PMSP_CSharpClient/PMSPClient/Track.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
-using System.IO;
-using System.Net;
-using NAudio.Wave;
 
 namespace PMSPClient
 {
@@ -18,18 +15,16 @@ namespace PMSPClient
         //Private fields.
         private string _title;
         private Artist _artist;
-        private string _url;
 
         //Public properties.
         public string Title { get { return _title; } }
         public string Artist { get { return _title; } }
 
         //Main constructor.
-        public Track(string title, Artist artist, string url)
+        public Track(string title, Artist artist)
         {
             _title = title;
             _artist = artist;
-            _url = url;
         }
 
         /// <summary>
@@ -51,8 +46,8 @@ namespace PMSPClient
             }
 
             /**********TEST DATA ONLY******************/
-            tracks.Add(new Track("Intolerance", new Artist("Tool", ""), @"C:\PMSPClient\PMSPClient\Tracks\Intolerance.mp3"));
-            tracks.Add(new Track("Lateralus", new Artist("Tool", ""), @"C:\PMSPClient\PMSPClient\Tracks\Lateralus.mp3"));
+            tracks.Add(new Track("Intolerance", new Artist("Tool", "")));
+            tracks.Add(new Track("Lateralus", new Artist("Tool", "")));
             /******************************************/
 
             //Return list.
@@ -65,38 +60,11 @@ namespace PMSPClient
         /// <param name="protocol"></param>
         public void Stream(Protocol protocol)
         {
-            //Inform user of current streaming track.
-            Console.WriteLine("Now streaming " + this._title + " by " + this._artist.DisplayName + "..." + Environment.NewLine);
+            /*
+             * Stream track here.
+             */
 
-            //Instantiate memory stream for track.
-            using (Stream memoryStream = new MemoryStream())
-            {
-                //Get stream from url.
-                using (Stream stream = WebRequest.Create(this._url).GetResponse().GetResponseStream())
-                {
-                    byte[] buffer = new byte[32768];
-                    int read;
-                    while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        memoryStream.Write(buffer, 0, read);
-                    }
-                }
-
-                //Use NAudio library to stream track.
-                memoryStream.Position = 0;
-                using (WaveStream blockAlignedStream = new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(memoryStream))))
-                {
-                    using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
-                    {
-                        waveOut.Init(blockAlignedStream);
-                        waveOut.Play();
-                        while (waveOut.PlaybackState == PlaybackState.Playing)
-                        {
-                            System.Threading.Thread.Sleep(100);
-                        }
-                    }
-                }
-            }
+            Console.WriteLine("Now streaming " + this.Title + " by " + this._artist.DisplayName + "..." + Environment.NewLine);
         }
     }
 }
