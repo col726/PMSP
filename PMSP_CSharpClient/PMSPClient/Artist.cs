49a81f287e0fba2dac23e190dbcef0ffa1d7786a
ColinM <col726@gmail.com>
2013-05-20 20:20:43 -0400

    Adding client code

---
+++ PMSP_CSharpClient/PMSPClient/Artist.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace PMSPClient
+{
+    /// <summary>
+    /// This is the Artist class used for audio artists.
+    /// </summary>
+    class Artist
+    {
+        //Private fields.
+        string _firstNameOrBandName;
+        string _lastName;
+        List<Track> _tracks;
+
+        //Public properties.
+        public string FirstName { get { return _firstNameOrBandName; } }
+        public string LastName { get { return _lastName; } }
+        public string DisplayName { get { return (_firstNameOrBandName + " " + _lastName).Trim(); } }
+        public List<Track> Tracks { get { return _tracks; } set { _tracks = value; } }
+
+        //Main constructor.
+        public Artist(string firstNameOrBandName, string lastName)
+        {
+            _firstNameOrBandName = firstNameOrBandName;
+            _lastName = lastName;
+        }
+
+        //Get list of artists.
+        public static List<Artist> GetList(Protocol protocol)
+        {
+            //Instantiate artist list.
+            List<Artist> artists = new List<Artist>();
+
+            //Insert artists.
+            foreach(XmlNode artist in protocol.GetList(ListType.Artist).ChildNodes)
+            {
+                /*
+                 * Add artists to list here.
+                 */
+            }
+
+            /**********TEST DATA ONLY******************/
+            artists.Add(new Artist("Rob", "Zombie"));
+            artists.Add(new Artist("Tool", ""));
+            /******************************************/
+
+            //Return list.
+            return artists;
+        }
+    }
+}
---
+++ PMSP_CSharpClient/PMSPClient/Track.cs
@@ -0,0 +1,70 @@
+﻿/*=======================Directives and Pragmas=============================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace PMSPClient
+{
+    /// <summary>
+    /// This is the Track class used for representing mp3 audio files as objects.
+    /// </summary>
+    class Track
+    {
+        //Private fields.
+        private string _title;
+        private Artist _artist;
+
+        //Public properties.
+        public string Title { get { return _title; } }
+        public string Artist { get { return _title; } }
+
+        //Main constructor.
+        public Track(string title, Artist artist)
+        {
+            _title = title;
+            _artist = artist;
+        }
+
+        /// <summary>
+        /// Gets a list of tracks from the server.
+        /// </summary>
+        /// <param name="artist">Optional artist parameter used to specify if only the tracks from a specific artists should be returned.</param>
+        /// <returns></returns>
+        public static List<Track> GetList(Protocol protocol, Artist artist)
+        {
+            //Instantiate track list.
+            List<Track> tracks = new List<Track>();
+
+            //Insert tracks.
+            foreach (XmlNode track in protocol.GetList(ListType.Track).ChildNodes)
+            {
+                /*
+                 * Insert tracks here.
+                 */
+            }
+
+            /**********TEST DATA ONLY******************/
+            tracks.Add(new Track("Intolerance", new Artist("Tool", "")));
+            tracks.Add(new Track("Lateralus", new Artist("Tool", "")));
+            /******************************************/
+
+            //Return list.
+            return tracks;
+        }
+
+        /// <summary>
+        /// Streams a track using the PMSP protocol.
+        /// </summary>
+        /// <param name="protocol"></param>
+        public void Stream(Protocol protocol)
+        {
+            /*
+             * Stream track here.
+             */
+
+            Console.WriteLine("Now streaming " + this.Title + " by " + this._artist.DisplayName + "..." + Environment.NewLine);
+        }
+    }
+}
