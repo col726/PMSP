9a4796c688407dffacdd35124f51953ad93dc594
Josh Krupka <jkrupka@nb-jkrupka-4.burrisfoods.com>
2013-05-29 00:47:49 -0400

    authentication, cookies, logging, etc

---
+++ PMSP_JavaServer/src/org/pmsp/domain/Retrieval.java
@@ -0,0 +1,15 @@
+package org.pmsp.domain;
+
+import java.util.List;
+
+public class Retrieval {
+	private List<MediaFile> mediaFiles;
+
+	public List<MediaFile> getMediaFiles() {
+		return mediaFiles;
+	}
+
+	public void setMediaFiles(List<MediaFile> mediaFiles) {
+		this.mediaFiles = mediaFiles;
+	}
+}
