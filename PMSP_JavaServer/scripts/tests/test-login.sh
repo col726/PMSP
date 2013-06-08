070c09b551bacf1adeb2ef1339eeaf6e3d5a8ef3
Josh Krupka <jkrupka@gmail.com>
2013-06-08 08:35:27 -0400

    add comments to scripts

--- PMSP_JavaServer/scripts/tests/test-login.sh
+++ PMSP_JavaServer/scripts/tests/test-login.sh
@@ -3,5 +3,6 @@
 #this script sends the login message to the server
 
 echo "Starting request for $$"
+# Can change -s to -v if you want to see all the data in stdout
 curl  -s -H "PMSP-Version: 1.0"  -X POST --data-binary @../../test-xml/LoginRequest.xml --user josh:josh  localhost:31415
 echo "Ending request for $$"
--- PMSP_JavaServer/scripts/tests/test-retrieve.sh
+++ PMSP_JavaServer/scripts/tests/test-retrieve.sh
@@ -3,5 +3,6 @@
 # This script uses curl to interact with the server
 
 echo "Starting request for $$"
-curl -s -H "PMSP-Version: 1.0"  -b "pmsp-sessionid=josh;pmsp-state=wait.file.choice" -o output.`echo $$` -X POST --data-binary @../../test-xml/RetrievalRequest.xml  localhost:31415
+# Can change -s to -v if you want to see all the data in stdout. The (large) xml file will be writeen to ./output.$$.xml
+curl -s -H "PMSP-Version: 1.0"  -b "pmsp-sessionid=josh;pmsp-state=wait.file.choice" -o output.`echo $$`.xml -X POST --data-binary @../../test-xml/RetrievalRequest.xml  localhost:31415
 echo "Ending request for $$"
